2d0df25e76aa52d200059a65f66099d4b26d1594
stefnotch <brandmairstefan@gmail.com>
2022-07-10 08:55:40 +0200

    VSCodeLauncher

--- README.md
+++ README.md
@@ -8,16 +8,40 @@
   - `PowerToys.ManagedCommon`
   - `Wox.Infrastructure`
   - `Wox.Plugin`
-3. Copy them to a `libs` folder and add a reference to them. Sadly they aren't on Nuget, so we'll have to make do with this cave-people approach. With Visual Studio, you do this by adding a "Project Reference" and clicking on "Browse".
+3. Copy them to a `libs` folder and add a reference to them. Sadly they aren't on Nuget, so we'll have to make do with this cave-people approach. With Visual Studio, you do this by adding a "Project Reference" and clicking on "Browse". 
 4. Check out plugins such as 
   - https://github.com/microsoft/PowerToys/tree/main/src/modules/launcher/Plugins
   - https://github.com/skttl/ptrun-guid
   - https://github.com/hlaueriksson/GEmojiSharp/tree/master/src/GEmojiSharp.PowerToysRun
 5. Build the plugin
-6. Copy it to a folder like "C:\Program Files\PowerToys\modules\launcher\Plugins\StefToys"
-7. Automate the copy-ing with `xcopy /E /Y "$(TargetDir)" "C:\Program Files\PowerToys\modules\launcher\Plugins\StefToys"`
+6. Copy it to a folder like "C:\Program Files\PowerToys\modules\launcher\Plugins\NAME_GOES_HERE"
+7. Automate the copy-ing with `xcopy /E /Y "$(TargetDir)" "C:\Program Files\PowerToys\modules\launcher\Plugins\NAME_GOES_HERE"`
   - Note that using `$(TargetDir)` means that we don't have to worry about accidentally copying the wrong build, like copying the debug build when we actually want to try out the release build.
+8. If it fails, that usually means that the Windows folder permissions are wrong. In that case, just create the correct folder and let users modify it.
 
 ## Documentation
 
 https://github.com/microsoft/PowerToys/tree/main/doc/devdocs/modules/launcher
+
+## Tasks
+
+Powertoy run
+- [x] open vscode in current directory 
+- [ ] yt-dl "url" (only available if yt-dl is on the path)
+- [x] ip: shows current local ip https://github.com/microsoft/PowerToys/issues/19284
+- [ ] brighter, darker, fullbright: integrated and use the current monitor, also shows the current brightness in the text
+
+
+Add custom command plugin
+- [ ] %appdata% & %temp%
+- [ ] start file HTTP server using current directory 
+
+Extra
+- [ ] alarm clock
+- [ ] do not disturb
+- [ ] brightness: gives you a proper brightness slider or two key to press to quickly adjust the brightness 
+- [ ] file browser (uses current explorer window)
+- [ ] Music lyrics and title search
+- [ ] color converter?
+- [ ] better calculator?
+- [ ] add to (Google) calendar
---
+++ VSCodeLauncher/Main.cs
@@ -0,0 +1,76 @@
+﻿using System.Runtime.InteropServices;
+using Wox.Infrastructure;
+using Wox.Plugin;
+
+namespace StefToys
+{
+    public class Main : IPlugin
+    {
+        private PluginInitContext? Context { get; set; }
+
+        public string Name => "VS Code Launcher";
+        public string Description => "Opens Visual Studio Code in the currently open directory";
+
+        public void Init(PluginInitContext context)
+        {
+
+            Context = context;
+        }
+
+        public static string TrimEndToLength(string text, int maxLength)
+        {
+            string Ellipses = "…";
+            if (maxLength < Ellipses.Length) return text;
+
+            maxLength -= Ellipses.Length;
+            if (text.Length <= maxLength) return text;
+
+            int charactersToTrim = text.Length - maxLength;
+            return Ellipses + text[charactersToTrim..];
+        }
+
+        public List<Result> Query(Query query)
+        {
+            var openExplorerPaths = GetOpenExplorerPaths();
+
+            int maxPathLength = 28;
+            return openExplorerPaths
+                .Select(path => new Result()
+                {
+                    Title = "Open VS Code in " + TrimEndToLength(path, maxPathLength),
+                    Action = e =>
+                    {
+                        Helper.OpenInShell("code", ".", path, runWithHiddenWindow: true);
+                        return true;
+                    }
+                })
+                .ToList();
+        }
+
+        public static IEnumerable<string> GetOpenExplorerPaths()
+        {
+            Type? type = Type.GetTypeFromProgID("Shell.Application");
+            if (type == null) yield break;
+            dynamic? shell = Activator.CreateInstance(type);
+            if (shell == null) yield break;
+            try
+            {
+                var openWindows = shell.Windows();
+                for (int i = 0; i < openWindows.Count; i++)
+                {
+                    var window = openWindows.Item(i);
+                    if (window == null) continue;
+                    var fileName = Path.GetFileName((string)window.FullName);
+                    if (fileName.ToLower() == "explorer.exe")
+                    {
+                        yield return new Uri(window.LocationURL).LocalPath; // window.document.focuseditem.path;
+                    }
+                }
+            }
+            finally
+            {
+                Marshal.FinalReleaseComObject(shell);
+            }
+        }
+    }
+}
